7b9bd20312371630a91ba170904ccf5360821fcc
unknown <rezakhakpoorboraz@gmail.com>
2021-03-28 00:32:25 -0700

    infrastructure-layer-added

---
+++ Shop.Infrastructure/Application/Service.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.Infrastructure.Application
+{
+    public interface Service
+    {
+    }
+}
---
+++ Shop.Infrastructure/Application/UnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.Infrastructure.Application
+{
+    public interface UnitOfWork
+    {
+        public void Complete();
+    }
+}
