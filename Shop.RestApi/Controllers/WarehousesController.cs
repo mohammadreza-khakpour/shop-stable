be70c3f0ca7bc61c92411fe9880521d9e8310fec
unknown <rezakhakpoorboraz@gmail.com>
2021-03-28 02:48:18 -0700

    warehouses-controller-added

---
+++ Shop.RestApi/Controllers/WarehousesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Services.Warehouses.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.RestApi.Controllers
+{
+    [ApiController]
+    [Route("api/warehouses")]
+    public class WarehousesController : Controller
+    {
+        private WarehouseService _service;
+        public WarehousesController(WarehouseService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public List<GetWarehousesGroupedByProductIdDto> GetAll()
+        {
+            return _service.GetAll();
+        }
+    }
+}
